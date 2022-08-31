7eb20730f13a1210491726b58c26be306c1e995e
Glen Adamson <glenadamson1984@gmail.com>
2022-08-31 21:57:02 +0100

    draft for the sub header title needs cleaned and then rolled out onto the other pages

--- pages/services.jsx
+++ pages/services.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { colours } from "../src/utils/style.utils";
 
 const Services = () => {
   return (
-    <div
-      style={{
-        height: "600px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      Services Page - Currently under construction
-    </div>
+    <>
+      <div
+        style={{
+          height: "200px",
+          background: `#262626`,
+          textAlign: "center",
+          font: "normal normal bold 48px/63px Roboto",
+          letterSpacing: "0px",
+          color: "#FFFFFF",
+          opacity: "1",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>Services</div>
+        <div
+          style={{
+            width: "100px",
+            borderBottom: "5px solid",
+            borderBottomColor: `${colours.red}`,
+          }}
+        />
+      </div>
+      <div
+        style={{
+          height: "500px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Services Page - Currently under construction
+      </div>
+    </>
   );
 };
 
