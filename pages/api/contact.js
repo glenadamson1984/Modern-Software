e63e73b077c68aabefa3c88d56c264a7581b9b43
Glen Adamson <glenadamson1984@gmail.com>
2022-09-11 14:42:13 +0100

    adding a promise to see if this helps

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   // if (req.method !== "POST") {
   //   res.status(404).json({ message: "Error sending email." });
   // }
@@ -28,13 +28,19 @@ export default function handler(req, res) {
     html: `<p>Name: ${name}</p><p>Phone: ${phone}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
   };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if (err)
-      res
-        .status(502)
-        .json({
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) {
+        reject(err);
+        res.status(502).json({
           message: `Error sending email. ${process.env.PASSWORD} ${err}`,
         });
-    else res.status(200).json({ name: "Message sent successfully." });
+      } else {
+        resolve(info);
+        res.status(200).json({ name: "Message sent successfully." });
+      }
+    });
+
+    res.status(200).json({ name: "Message sent successfully." });
   });
 }
