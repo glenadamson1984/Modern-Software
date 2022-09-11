9a3f1271b672fd5046ba845cc9b85fb833da88a5
Glen Adamson <glenadamson1984@gmail.com>
2022-09-11 18:18:29 +0100

    trying with sendgrid since vercel blocks all outgoing smtp traffic

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -22,45 +22,49 @@ export default async function handler(req, res) {
     .send(msg)
     .then(() => {
       console.log("Email sent");
+      res.status(200).json({ name: "Message sent successfully." });
     })
     .catch((error) => {
       console.error(error);
+      res.status(502).json({
+        message: `Error sending email. ${process.env.PASSWORD} ${err}`,
+      });
     });
 
-  const { name, phone, email, message } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-      user: "modernsoftware@hotmail.com",
-      pass: process.env.PASSWORD,
-    },
-  });
-
-  const mailData = {
-    from: "modernsoftware@hotmail.com",
-    to: "glenadamson1984@gmail.com",
-    subject: "Modern Software Generated Enquiry",
-    text: `Name ${name}
-    Phone: ${phone}
-    Email: ${email}, 
-    Message: ${message}`,
-    html: `<p>Name: ${name}</p><p>Phone: ${phone}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
-  };
-
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailData, function (err, info) {
-      if (err) {
-        reject(err);
-        res.status(502).json({
-          message: `Error sending email. ${process.env.PASSWORD} ${err}`,
-        });
-      } else {
-        resolve(info);
-        res.status(200).json({ name: "Message sent successfully." });
-      }
-    });
-
-    res.status(200).json({ name: "Message sent successfully." });
-  });
+  // const { name, phone, email, message } = req.body;
+  //
+  // const transporter = nodemailer.createTransport({
+  //   service: "hotmail",
+  //   auth: {
+  //     user: "modernsoftware@hotmail.com",
+  //     pass: process.env.PASSWORD,
+  //   },
+  // });
+  //
+  // const mailData = {
+  //   from: "modernsoftware@hotmail.com",
+  //   to: "glenadamson1984@gmail.com",
+  //   subject: "Modern Software Generated Enquiry",
+  //   text: `Name ${name}
+  //   Phone: ${phone}
+  //   Email: ${email},
+  //   Message: ${message}`,
+  //   html: `<p>Name: ${name}</p><p>Phone: ${phone}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  // };
+  //
+  // await new Promise((resolve, reject) => {
+  //   transporter.sendMail(mailData, function (err, info) {
+  //     if (err) {
+  //       reject(err);
+  //       res.status(502).json({
+  //         message: `Error sending email. ${process.env.PASSWORD} ${err}`,
+  //       });
+  //     } else {
+  //       resolve(info);
+  //       res.status(200).json({ name: "Message sent successfully." });
+  //     }
+  //   });
+  //
+  //
+  // });
 }
