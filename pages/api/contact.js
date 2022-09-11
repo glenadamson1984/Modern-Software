a5725b384a06c5e9918b370931ad0f8d86889924
Glen Adamson <glenadamson1984@gmail.com>
2022-09-11 13:57:37 +0100

    adding email functionality - needs styling and validation

---
+++ pages/api/contact.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(404).json({ message: "Error sending email." });
+  }
+
+  const { name, phone, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+      user: "modernsoftware@hotmail.com",
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailData = {
+    from: "modernsoftware@hotmail.com",
+    to: "glenadamson1984@gmail.com",
+    subject: "Modern Software Generated Enquiry",
+    text: `Name ${name}
+    Phone: ${phone}
+    Email: ${email}, 
+    Message: ${message}`,
+    html: `<p>Name: ${name}</p><p>Phone: ${phone}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) res.status(502).json({ message: "Error sending email." });
+    else res.status(200).json({ name: "Message sent successfully." });
+  });
+}
--- pages/contactus.jsx
+++ pages/contactus.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SubPageLayout from "../src/components/layout/SubPageLayout";
 
 const ContactUs = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let data = {
+      name: nameRef.current.value,
+      phone: phoneRef.current.value,
+      email: emailRef.current.value,
+      message: messageRef.current.value,
+    };
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setSubmitted(true);
+          // we will throw up a success modal here
+          nameRef.current.value = "";
+          phoneRef.current.value = "";
+          emailRef.current.value = "";
+          messageRef.current.value = "";
+        }
+      })
+      .catch((e) => {
+        // we will throw up an alert modal here
+        console.log({ e });
+      });
+  };
+
   return (
     <SubPageLayout subTitle="Contact Us">
-      Currently under construction
+      <div>
+        Send A Message
+        <form>
+          <fieldset>
+            <label htmlFor="name">Name</label>
+            <input ref={nameRef} type="text" name="name" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="phone">Phone</label>
+            <input ref={phoneRef} type="text" name="phone" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="email">Email</label>
+            <input ref={emailRef} type="text" name="email" />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="message">Message</label>
+            <input ref={messageRef} type="text" name="message" />
+          </fieldset>
+          <input
+            type="submit"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          />
+        </form>
+      </div>
     </SubPageLayout>
   );
 };
