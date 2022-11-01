59471c115e6f2f685428d0e1faa5c0aeaad34aac
Glen Adamson <glenadamson1984@gmail.com>
2022-11-01 10:11:42 +0000

    adding loughgall to portfolio.jsx

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -15,6 +15,12 @@ const Portfolio = () => {
         imageUrl="/images/planningportal.png"
         description="We delivered a fully frontend service to help build out the new planning portal website for Northern Ireland. Due for release in 2023 - ReactJS, NextJS, StoryBook"
       />
+      <ImageCard
+        position="left"
+        title="Loughall Youth FC"
+        imageUrl="/images/loughgall.jpg"
+        description="Due to the ever increasing popularity of Loughgall Youth F.C the administration to run the club was stopping them from spending the time where they needed it most. Following a few seasons of struggling with paper consent forms they approached use to fully automate this experience saving endless hours for this year and many more to come."
+      />
     </SubPageLayout>
   );
 };
