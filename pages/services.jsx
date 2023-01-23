5047a1e8908f63d6dc851e579b490f3b417dc9e8
Bilal Saleem <bilalsaleem.dev@gmail.com>
2023-01-24 02:51:21 +0500

    Services Page Update with UI and stripe integration

--- pages/services.jsx
+++ pages/services.jsx
@@ -55,57 +55,42 @@ const Services = () => {
         (
 
           <div className="grid gap-20 mt-40">
-        <div className="case-study u-pad-bottom-x3 u-pad-bottom-x4@m u-pad-bottom-x5@l">
-          <div className="o-wrap">
-            <div className="c-card +default +featured">
+            <div className="case-study u-pad-bottom-x3 u-pad-bottom-x4@m u-pad-bottom-x5@l">
+              <div className="o-wrap">
+                <div className="c-card +default +featured">
 
-              <a href="" className="c-media-box +offset">
-                <img src={`http://localhost:1337${item.attributes.service_image.data.attributes.url}`} />
-              </a>
-
-              <div className="c-card__content +centered">
-                <div className="u-marg-bottom">
-                  <span className="c-pill u-fill-primary">Success Story</span>
-                </div>
-                <h2 className="c-heading +h3">
-                  <a href="">
-                  {item.attributes.title}
+                  <a href="" className="c-media-box +offset">
+                    <img src={`http://localhost:1337${item.attributes.service_image.data.attributes.url}`} />
                   </a>
-                </h2>
-                <p className="u-beta@m">
-                {item.attributes.service_description}
-                </p>
-                {/* <p className="u-lighten">
+
+                  <div className="c-card__content +centered">
+                    <div className="u-marg-bottom">
+                      <span className="c-pill u-fill-primary">Service</span>
+                    </div>
+                    <h2 className="c-heading +h3">
+                      <a href="">
+                        {item.attributes.title}
+                      </a>
+                    </h2>
+                    <p className="u-beta@m">
+                      {item.attributes.service_description}
+                    </p>
+                    {/* <p className="u-lighten">
                   Mobile App Development,
                   Native iOS Android Apps,
                   Platform Integrations,
                   UX/UI Design
                 </p> */}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-
-          // <div className="container-fluid d-flex">
-          //   <div className="row border">
-          //     <div className="col-6">
-          //       <div className="img">
-          //         <img src={`http://localhost:1337${item.attributes.service_image.data.attributes.url}`} width="100%" />
-          //       </div>
-          //     </div>
-          //     <div className="col-6">
-          //       <span className="nav-link bg-dark text-light w-50 p-1 mt-2 text-center">success story</span>
-          //       <h2 className="h-2 mt-2">{item.attributes.title}</h2>
-          //       <p className="text-dark">{item.attributes.service_description}</p>
-          //     </div>
-          //   </div>
-          // </div>
         )
         )
 
       }
-      <div style={{ display: "flex", justifyContent: "center", marginBottom:"40px" }}>
+      {/* <div style={{ display: "flex", justifyContent: "center", marginBottom: "40px" }}>
         <CallToActionButton
           onClick={async (e) => {
             // await handleSubmit(e);
@@ -113,11 +98,27 @@ const Services = () => {
         >
           Expand
         </CallToActionButton>
-      </div>
+      </div> */}
 
       {/* <StyledRow>
+        <ServiceCard
+          imageUrl="/images/10168.jpg"
+          title="Custom Software Development"
+        />
+        <ServiceCard
+          imageUrl="/images/45.jpg"
+          title="Web Application Development"
+        />
+      </StyledRow>
+      <StyledRow>
+        <ServiceCard imageUrl="/images/42.jpg" title="Mobile App Development" />
+        <ServiceCard imageUrl="/images/36.jpg" title="UX & UI Design" />
+      </StyledRow>
+      <StyledRow>
         <ServiceCard imageUrl="/images/8600.jpg" title="Software Tuition" />
       </StyledRow> */}
+
+
     </SubPageLayout>
   );
 };
