1e32f799db8c2a5f994ab0129c8b579c84fc542a
Glen Adamson <glenadamson1984@gmail.com>
2023-04-14 10:25:04 +0100

    fixing services

--- pages/services.jsx
+++ pages/services.jsx
@@ -23,24 +23,28 @@ export const StyledRow = styled.div`
 `;
 const Services = () => {
   const { pathname } = useRouter();
-  const [servicesData, setServicesData] = useState([])
+  const [servicesData, setServicesData] = useState([]);
 
   useEffect(() => {
-    fetchServices()
-  }, [])
-
+    fetchServices();
+  }, []);
 
   const fetchServices = async () => {
-
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/services?populate=*`, {
-        method: "GET",
-        headers: { Accept: "application/json, text/plain, */*", "Content-Type": "application/json" }
-      });
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/services?populate=*`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.status === 200) {
         const res = await response?.json();
-        setServicesData(res?.data)
+        setServicesData(res?.data);
       }
     } catch (e) {
       toast.error("Please try again later.", { theme: "colored" });
@@ -49,45 +53,36 @@ const Services = () => {
 
   return (
     <SubPageLayout subTitle={"What we do"}>
-      {
-        servicesData?.map((item) =>
-        (
-
-          <div className="grid gap-20 mt-40">
-            <div className="case-study u-pad-bottom-x3 u-pad-bottom-x4@m u-pad-bottom-x5@l">
-              <div className="o-wrap">
-                <div className="c-card +default +featured">
-
-                  <a href="" className="c-media-box +offset">
-                    <img src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${item?.attributes?.service_image?.data?.attributes?.url}`} />
-                  </a>
-
-                  <div className="c-card__content +centered">
-                    <div className="u-marg-bottom">
-                      <span className="c-pill u-fill-primary">Service</span>
-                    </div>
-                    <h2 className="c-heading +h3">
-                      <a href="">
-                        {item?.attributes?.title}
-                      </a>
-                    </h2>
-                    <p className="u-beta@m">
-                      {item?.attributes?.service_description}
-                    </p>
-                    <p className="u-lighten">
-                  Mobile App Development,
-                  Native iOS Android Apps,
-                  Platform Integrations,
-                  UX/UI Design
-                </p>
+      {servicesData?.map((item, index) => (
+        <div key={index} className="grid gap-20 mt-40">
+          <div className="case-study u-pad-bottom-x3 u-pad-bottom-x4@m u-pad-bottom-x5@l">
+            <div className="o-wrap">
+              <div className="c-card +default +featured">
+                <a href="" className="c-media-box +offset">
+                  <img
+                    src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${item?.attributes?.service_image?.data?.attributes?.url}`}
+                  />
+                </a>
+                <div className="c-card__content +centered">
+                  <div className="u-marg-bottom">
+                    <span className="c-pill u-fill-primary">Service</span>
                   </div>
+                  <h2 className="c-heading +h3">
+                    <a href="">{item?.attributes?.title}</a>
+                  </h2>
+                  <p className="u-beta@m">
+                    {item?.attributes?.service_description}
+                  </p>
+                  <p className="u-lighten">
+                    Mobile App Development, Native iOS Android Apps, Platform
+                    Integrations, UX/UI Design
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        )
-        )
-      }
+        </div>
+      ))}
       {/* <div style={{ display: "flex", justifyContent: "center", marginBottom: "40px" }}>
         <CallToActionButton
           onClick={async (e) => {
@@ -115,8 +110,6 @@ const Services = () => {
       <StyledRow>
         <ServiceCard imageUrl="/images/8600.jpg" title="Software Tuition" />
       </StyledRow> */}
-
-
     </SubPageLayout>
   );
 };
